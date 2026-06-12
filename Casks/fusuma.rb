83d934715076a3751e80ba75feff36d28cf7f3ba
cOridrew <andrew.tech.developer@gmail.com>
2026-06-12 10:11:05 +0900

    fusuma 1.0.21

--- Casks/fusuma.rb
+++ Casks/fusuma.rb
@@ -1,6 +1,6 @@
 cask "fusuma" do
-  version "1.0.20"
-  sha256 "c48aacae772ec086895349c959ef8ec9936f645d445c297958a5e22cae6f3226"
+  version "1.0.21"
+  sha256 "3436f3fb48db5d01a3edb4cb8f52881db6e8bc123f368bc2718339be249c4923"
 
   url "https://api.fusuma.dev/updates/Fusuma-#{version}.dmg"
   name "Fusuma"
